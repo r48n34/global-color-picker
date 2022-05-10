7926a069275c911dd478929d9271a8771a99cd92
reemo <r48n34@gmail.com>
2022-05-11 00:29:21 +0800

    fix bug in react web

--- popup.js
+++ popup.js
@@ -19,16 +19,11 @@ async function openEyesDrop() {
 
       document.body.removeChild(textarea);
 
-      const myPopBox = `
-        <div id="popUpColorAlert" style="font-family: sans-serif; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; display:flex; align-items:center; justify-content:center; border-radius: 20px; position: fixed; width: 200px; height: 60px; bottom: 3%; left:45%; background-color:#FFFFFF">
-          <div>
-          <h5 style="color:#000000; text-align:center; margin:0; font-size: 8px;">Success to copy in clipboard.</h5>
-          <h5 style="text-align:center; margin:0; font-size: 8px; text-shadow: 2px 2px 3px rgba(0,0,0,0.4); color:${colorHexValue};">${colorHexValue}</h5>
-          </div>
-        </div>
-      `
-
-      document.body.innerHTML += myPopBox;
+      const popUpDiv = document.createElement("div");
+      popUpDiv.textContent = `Copied ${colorHexValue}`;
+      popUpDiv.id = "popUpColorAlert";
+      popUpDiv.style.cssText = "color:#000000; font-family: sans-serif; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; display:flex; align-items:center; justify-content:center; border-radius: 20px; position: fixed; width: 200px; height: 60px; bottom: 3%; left:45%; background-color:#FFFFFF"
+      document.body.appendChild(popUpDiv);
       setTimeout( () => document.getElementById('popUpColorAlert').remove(), 1800)
 
     } catch (err) {
