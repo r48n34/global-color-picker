17f3e1306cc0e5221fc4cf550eec38cb7ff707a8
reemo <r48n34@gmail.com>
2022-05-10 23:59:16 +0800

    remove sweetalert dependents

--- popup.js
+++ popup.js
@@ -17,40 +17,27 @@ async function openEyesDrop() {
       textarea.select();
       document.execCommand("copy");
 
-      Swal.fire({
-        position: 'bottom',
-        html: `
-        <h5 style="margin:0; font-size: 8px;">Success to copy in clipboard.</h5>
-        <h5 style="margin:0; font-size: 8px; text-shadow: 2px 2px 3px rgba(0,0,0,0.4); color:${colorHexValue};">${colorHexValue}</h5>
-        `,
-        showConfirmButton: false,
-        backdrop: false,
-        timer: 1500,
-        width: "14rem",
-        padding:"1rem"
-      })
-      
       document.body.removeChild(textarea);
 
+      const myPopBox = `
+        <div id="popUpColorAlert" style="font-family: sans-serif; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; display:flex; align-items:center; justify-content:center; border-radius: 20px; position: fixed; width: 200px; height: 60px; bottom: 3%; left:45%; background-color:#FFFFFF">
+          <div>
+          <h5 style="color:#000000; text-align:center; margin:0; font-size: 8px;">Success to copy in clipboard.</h5>
+          <h5 style="text-align:center; margin:0; font-size: 8px; text-shadow: 2px 2px 3px rgba(0,0,0,0.4); color:${colorHexValue};">${colorHexValue}</h5>
+          </div>
+        </div>
+      `
+
+      document.body.innerHTML += myPopBox;
+      setTimeout( () => document.getElementById('popUpColorAlert').remove(), 1800)
+
     } catch (err) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: "Can not use here.",
-      })
       return;
-    }
-    finally{
-      document.body.removeChild(textarea);
     }
 
   }
   else{
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Your browser is not support EyeDropper. See https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API for more informations.',
-    })
+    console.log("Oh no");
   }
 }
 
